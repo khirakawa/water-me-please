65c0dd6017389653cc22a854e7960cc6a7bbc4a7
khirakawa <k.hirakawa88@gmail.com>
2013-03-09 18:23:22 -0500

    added logic to tweet when in need of water

--- app.js
+++ app.js
@@ -4,21 +4,31 @@ var events = require('events'),
   arduino = require('duino'),
   nconf = require('nconf'),
   twitter = require('ntwitter'),
+  moment = require('moment'),
   reading = false,
   self = this;
 
 // read in configuration
-// read in command line args
 nconf.argv().env().file({ file: 'config.json' });
+
+// helper method to get mean of an array
+var getMean = function (arr) {
+  return _.reduce(arr, function (sum, item) {
+    return sum + item;
+  }, 0) / arr.length;
+};
+
 
 // our water level sensor
 var WaterLevelSensor = function (options) {
   options = options || {};
-  this.reading = false;
   this.debug = options.debug || false;
   this.pin = options.pin || 7;
   this.sensor_pin = options.sensor_pin || 'A0';
   this.baudrate = options.baudrate || 9600;
+
+  // This will be used to determine when to tweet
+  this.IM_DYING_THRESHOLD = 520;
 
   var self = this;
 
@@ -37,13 +47,6 @@ var WaterLevelSensor = function (options) {
     // set pin to out mode
     self.board.pinMode(self.pin, 'out');
 
-    // read from sensor
-    self.sensor.on('read', function (err, value) {
-      if (self.reading) {
-        self.emit('measurementTaken', value);
-      }
-    });
-
     self.emit('ready');
   });
 };
@@ -51,19 +54,33 @@ var WaterLevelSensor = function (options) {
 // make it emitterable
 util.inherits(WaterLevelSensor, events.EventEmitter);
 
-// add the enableSensor method
-WaterLevelSensor.prototype.measure = function (ms) {
-  console.log("enabling sensor");
-  var self = this;
-  this.reading = true;
+// measures for 5 reads and calls callback with average value
+WaterLevelSensor.prototype.measure = function (callback) {
+  var self = this,
+    readCount = 0,
+    measurements = []; // running list of measurements
 
   // send 5V to pin for 'ms' milliseconds
   this.board.digitalWrite(this.pin, this.board.HIGH);
 
-  setTimeout(function () {
-    self.board.digitalWrite(self.pin, self.board.LOW);
-    self.reading = false;
-  }, ms);
+  // read from sensor
+  var readCallback = function (err, value) {
+    measurements.push(parseInt(value, 10));
+
+    if (measurements.length === 5) {
+      var mean = getMean(measurements);
+
+      // reset mode
+      self.board.digitalWrite(self.pin, self.board.LOW);
+      self.sensor.removeListener('read', readCallback);
+
+      // we're finished, evoke the callback
+      callback(mean);
+    }
+  };
+
+  this.sensor.on('read', readCallback);
+
 };
 
 // set twitter
@@ -88,49 +105,48 @@ var TwitterUpdater = function (options) {
 // make it emitterable
 util.inherits(TwitterUpdater, events.EventEmitter);
 
-TwitterUpdater.prototype.updateStatus = function (msg) {
+TwitterUpdater.prototype.tweet = function (msg) {
   this.twit.updateStatus(msg,
     function (err, data) {
       if (err) {
         console.log("failed update status. error:", err);
+      } else {
+        console.log("tweeted:", msg);
       }
     }
   );
 };
 
-var getMean = function (arr) {
-  return _.reduce(arr, function (sum, item) {
-    return sum + item;
-  }, 0) / arr.length;
-};
-
 // first setup twitter
 var twitterUpdater = new TwitterUpdater(nconf.get("twitter"));
 twitterUpdater.on('ready', function () {
-  // now setup the sensor
-  var sensor = new WaterLevelSensor(nconf.get("arduino")),
-    measurements = []; // running list of measurements.
 
+  // now setup the sensor
+  var sensor = new WaterLevelSensor(nconf.get("arduino"));
+
+  // on read state,
   sensor.on('ready', function () {
-    console.log("ready!");
+
+    var interval = nconf.get('measure_interval') || 10000,
+      neglected = false;
 
     // take measurements at intervals
-    var interval = nconf.get('measure_interval') || 10000;
     setInterval(function () {
-      sensor.measure(2000);
+      sensor.measure(function (value) {
+
+        // twitter will block duplicate tweets, so we'll need to add some
+        // dates to make it unique.
+        var date = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+        // If plant goes from neglected to rescued or vice versa, tweet!
+        if (value > sensor.IM_DYING_THRESHOLD && !neglected) {
+          twitterUpdater.tweet("Yo Dawg, I need water. Neglected from " + date);
+          neglected = true;
+        } else if (value <= sensor.IM_DYING_THRESHOLD && neglected) {
+          twitterUpdater.tweet("Ah, that feels good.  Thanks for the water.  Rescued on " + date);
+          neglected = false;
+        }
+      });
     }, interval);
-  });
-
-  sensor.on('measurementTaken', function (value) {
-    measurements.push(parseInt(value, 10));
-
-    if (measurements.length === 5) {
-      var mean = getMean(measurements);
-      measurements.shift();
-
-      if(mean > 500){
-        // send tweet
-      }
-    }
   });
 });
